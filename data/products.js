2f5ce1c4eec9b0f336b6df170b925a21e247fbf4
kuyadaaa <magomedovdaniyal5@mail.ru>
2025-12-20 12:36:50 +0300

    backend practice

--- data/products.js
+++ data/products.js
@@ -716,5 +716,3 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
-console.log(products);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 //import "../../data/cart-oop.js";
+import "../data/backend-practice.js";
 
 renderPaymentSummary();
 renderOrderSummary();
